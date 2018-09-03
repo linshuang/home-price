2e223b92828a77a019695ab792bd0f02ea020bf7
linshuang <linshuang@baizeai.com>
2018-09-03 22:42:23 +0800

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from scipy.stats import norm
+from sklearn.preprocessing import StandardScaler
+from scipy import stats
+import warnings
+warnings.filterwarnings('ignore')
+
+df_train = pd.read_csv('./data/train.csv')
+print(df_train.columns)
+
+
+print('ddd')
